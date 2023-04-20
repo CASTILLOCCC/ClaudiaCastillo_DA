d447acaa42e22a5cfc9e8d85b8c776e8de9cf43f
unknown <cclaudiaximena@gmail.com>
2023-04-20 15:57:42 -0500

    actualizar

--- Config/database.php
+++ Config/database.php
@@ -2,7 +2,7 @@
 class Conexion
 {
     public static function Conexion(){
-        $conexion = new mysqli ("containers-us-west-135.railway.app","root","vk5gLrcvbRDaBsCKknL6","railway","6697");
+        $conexion = new mysqli ("containers-us-west-75.railway.app","root","vk5gLrcvbRDaBsCKknL6","railway","6697");
         return $conexion;
     }
 }
